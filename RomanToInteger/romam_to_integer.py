f7e3e14a7c0d184eca5ba5d3c60461ac6df7cca5
Ashish Rai <rai.coldcharm@gmail.com>
2022-11-06 21:59:58 +0530

    add 7 new problems - total 20

---
+++ RomanToInteger/romam_to_integer.py
@@ -0,0 +1,30 @@
+#! /usr/bin/env python3
+#! _*_coding: utf-8 _*_
+__author__ = "Ashish Rai"
+__title__ = "Roman to Integer"
+__license__ = "MIT"
+__copyright__ = "2022, Ashish Rai"
+
+"""
+ROMAN TO INTEGER
+================
+
+Difficulty level: Easy
+
+Roman numerals are represented using seven different symbols:
+    - I: 1
+    - V: 5
+    - X: 10
+    - L: 50
+    - C: 100
+    - D: 500
+    - M: 1000
+Roman numerals are usally written largest to smallest from left to right.
+Some however, such a 4 is written as IV instead of IIII. The same applies
+to number 9 written as IX.
+
+Input (str):
+    string representing roman numerial capitalised
+Output (int):
+    integer representing decimal form of romal numeral
+"""
